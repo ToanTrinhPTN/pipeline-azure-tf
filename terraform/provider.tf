224e8826eddc0a0308acbe8348c2b664e5e82dcf
Toan Trinh <toan.trinh@ptnglobalcorp.com>
2023-07-26 13:57:56 +0700

    asdasdad

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,7 +9,9 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {
-    # skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  }
+  features {}
+    subscription_id = "174ef490-128c-49fe-9921-77f1134a884a"
+    # client_id       = "a3cf1b56-abca-4eaa-b829-1afec52c6e4f"
+    # client_secret   = "elV8Q~oDSDkF2HlcdVBZducYbLZLGBwhO7Dymabe"
+    # tenant_id       = "b3da5948-48e5-42fc-8d9b-cd0f5a5c8ecb"
 }
