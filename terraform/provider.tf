4892389bdee93eaaa6223879e5e41ae23d194ad9
Toan Trinh <toan.trinh@ptnglobalcorp.com>
2023-07-27 09:27:25 +0700

    best

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,10 +10,11 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  # subscription_id = "90802f8d-10f6-49cb-880c-ac7ef25a9a63"
-  # client_id       = "1073d38a-7d48-410f-b8f3-ee65fb14d1cf"
-  # client_secret   = "UP-8Q~7VcUHarU-S6pG9Kt_Tjsrwf2UsSdbqNcMs"
-  # tenant_id       = "8b8f627d-40d6-4e75-9dcc-2650bf42d0ce"
+  
+  subscription_id = "90802f8d-10f6-49cb-880c-ac7ef25a9a63"
+  client_id       = "1073d38a-7d48-410f-b8f3-ee65fb14d1cf"
+  client_secret   = "UP-8Q~7VcUHarU-S6pG9Kt_Tjsrwf2UsSdbqNcMs"
+  tenant_id       = "8b8f627d-40d6-4e75-9dcc-2650bf42d0ce"
 }
 # az login --service-principal -u 1073d38a-7d48-410f-b8f3-ee65fb14d1cf -p UP-8Q~7VcUHarU-S6pG9Kt_Tjsrwf2UsSdbqNcMs --tenant 8b8f627d-40d6-4e75-9dcc-2650bf42d0ce
 
