825637cdeac3d7662aedbde39f3962fe656a37d8
Toan Trinh <toan.trinh@ptnglobalcorp.com>
2023-07-27 08:52:46 +0700

    test

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 }
-
+  
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
@@ -16,3 +16,6 @@ provider "azurerm" {
   tenant_id       = "8b8f627d-40d6-4e75-9dcc-2650bf42d0ce"
 }
 # az login --service-principal -u 1073d38a-7d48-410f-b8f3-ee65fb14d1cf -p UP-8Q~7VcUHarU-S6pG9Kt_Tjsrwf2UsSdbqNcMs --tenant 8b8f627d-40d6-4e75-9dcc-2650bf42d0ce
+
+# az ad sp create-for-rbac --name "myApp" --role contributor --scopes /subscriptions/90802f8d-10f6-49cb-880c-ac7ef25a9a63/resourceGroups/cloud-shell-storage-southeastasia --sdk-auth
+# az ad sp create-for-rbac --sdk-auth
